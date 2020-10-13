6040ef2d19e4566563f35f50e9eead535027e14f
Anmeet <anmeet619@yahoo.com>
2020-10-13 15:24:02 +1100

    add congrats testing

---
+++ joddo-tdd/src/Congrats.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * @function
+ * @returns {JSX.Element}
+ */
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congratulations! you guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats' />;
+  }
+};
