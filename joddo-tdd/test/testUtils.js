907c31d95054615ca78f364222aef9b1bb28a454
Anmeet <anmeet619@yahoo.com>
2020-10-13 21:55:38 +1100

    setup input component and store factory test

--- joddo-tdd/test/testUtils.js
+++ joddo-tdd/test/testUtils.js
@@ -1,10 +1,16 @@
 import checkPropTypes from 'check-prop-types';
+import {createStore} from 'redux';
+
+import rootReducer from '../src/reducers';
 /** Return node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper
  * @param {String} val - value of data-test attribute for search
  * @returns {shallowWrapper}
  */
 
+ export const storeFactory = (initialState) => {
+     return createStore(rootReducer, initialState);
+ }
  
 export const findByTestAttr = (wrapper, val) => {
    return wrapper.find(`[data-test="${val}"]`);
