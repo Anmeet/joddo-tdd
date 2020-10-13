0d8f32e91a2263c57879cc7badedba106c46027f
Anmeet <anmeet619@yahoo.com>
2020-10-13 13:40:30 +1100

    commit

--- joddo-tdd/src/App.test.js
+++ joddo-tdd/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+/** @function setup
+ * @returns {shallowWrapper}
+ */
+
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test('renders without crashing', () => {
+   const wrapper = setup();
+   const appComponent = findByTestAttr(wrapper,'component-app')
+   expect (appComponent.length).toBe(1);
+   //console.log(wrapper.debug())
 });
